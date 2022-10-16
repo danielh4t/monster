823cef5202ef207927d542fc4fb31dc640f0b99a
danielh4t <danielh4t@gmail.com>
2022-10-16 15:07:44 +0200

    add beast data model to represent a particular monster flavor

---
+++ app/src/main/java/app/stacq/monster/data/model/Beast.kt
@@ -0,0 +1,12 @@
+package app.stacq.monster.data.model
+
+data class Beast(
+    val name: String,
+    val product: String,
+    val flavor: String,
+    val flavorProfile: String
+) {
+
+    var rating: Int = 1
+    var unleashed: Boolean = false
+}
