96be4357678a84b78b6dd32b8bcf5564f1a9b61f
danielh4t <danielh4t@gmail.com>
2022-10-18 20:24:13 +0200

    add default beasts repository

--- app/src/main/java/app/stacq/monster/ui/beasts/BeastsViewModel.kt
+++ app/src/main/java/app/stacq/monster/ui/beasts/BeastsViewModel.kt
@@ -1,74 +1,9 @@
 package app.stacq.monster.ui.beasts
 
-
 import androidx.lifecycle.ViewModel
 import app.stacq.monster.data.model.Beast
 
 class BeastsViewModel : ViewModel() {
 
-    val beasts: List<Beast> = listOf(
-        Beast(
-            1,
-            "The Original",
-            "Monster Energy",
-            "The original green monster energy",
-            "sweet and salty"
-        ),
-        Beast(
-            2,
-            "The Original",
-            "Monster Energy",
-            "The original green monster energy",
-            "sweet and salty"
-        ),
-        Beast(
-            3,
-            "The Original",
-            "Monster Energy",
-            "The original green monster energy",
-            "sweet and salty"
-        ),
-        Beast(
-            4,
-            "The Original",
-            "Monster Energy",
-            "The original green monster energy",
-            "sweet and salty"
-        ),
-        Beast(
-            5,
-            "The Original",
-            "Monster Energy",
-            "The original green monster energy",
-            "sweet and salty"
-        ),
-        Beast(
-            6,
-            "The Original",
-            "Monster Energy",
-            "The original green monster energy",
-            "sweet and salty"
-        ),
-        Beast(
-            7,
-            "The Original",
-            "Monster Energy",
-            "The original green monster energy",
-            "sweet and salty"
-        ),
-        Beast(
-            8,
-            "The Original",
-            "Monster Energy",
-            "The original green monster energy",
-            "sweet and salty"
-        ),
-        Beast(
-            9,
-            "The Original",
-            "Monster Energy",
-            "The original green monster energy",
-            "sweet and salty"
-        )
-    )
+    val beasts: List<Beast> = listOf()
 }
