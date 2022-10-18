c2e5893df66ca685a6d0da593693aad13d57fc58
danielh4t <danielh4t@gmail.com>
2022-10-18 16:30:00 +0200

    remove rating and unleashed on beast model to represent flavor

--- app/src/main/java/app/stacq/monster/data/model/Beast.kt
+++ app/src/main/java/app/stacq/monster/data/model/Beast.kt
@@ -3,11 +3,7 @@ package app.stacq.monster.data.model
 data class Beast(
     val id: Int,
     val name: String,
-    val product: String,
     val flavor: String,
-    val flavorProfile: String
-) {
-
-    var rating: Int = 1
-    var unleashed: Boolean = false
-}
+    val profile: String,
+    val tag: String,
+)
