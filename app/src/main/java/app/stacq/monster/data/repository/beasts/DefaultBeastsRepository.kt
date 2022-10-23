c658b72de8a42b104b2a9b0be9ac10b6054de7dc
danielh4t <danielh4t@gmail.com>
2022-10-23 23:07:53 +0200

    add internet permission

--- app/src/main/java/app/stacq/monster/data/repository/beasts/DefaultBeastsRepository.kt
+++ app/src/main/java/app/stacq/monster/data/repository/beasts/DefaultBeastsRepository.kt
@@ -2,21 +2,28 @@ package app.stacq.monster.data.repository.beasts
 
 import app.stacq.monster.data.model.Beast
 import app.stacq.monster.data.repository.BeastsRepository
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ktx.database
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.snapshots
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
 
 class DefaultBeastsRepository(
-    private val database: FirebaseDatabase = Firebase.database,
+    private val database: FirebaseFirestore = Firebase.firestore,
     private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : BeastsRepository {
 
 
     override suspend fun getBeasts() = withContext(ioDispatcher) {
         return@withContext emptyList<Beast>()
+    }
+
+    fun getFlavors(): Flow<DocumentSnapshot> {
+        return database.collection("flavors").document("0").snapshots()
     }
 
 }
--- app/src/main/java/app/stacq/monster/ui/beasts/BeastsViewModel.kt
+++ app/src/main/java/app/stacq/monster/ui/beasts/BeastsViewModel.kt
@@ -1,9 +1,21 @@
 package app.stacq.monster.ui.beasts
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import app.stacq.monster.data.model.Beast
+import app.stacq.monster.data.repository.beasts.DefaultBeastsRepository
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class BeastsViewModel : ViewModel() {
 
     val beasts: List<Beast> = listOf()
+
+    init {
+        val flavors = DefaultBeastsRepository().getFlavors()
+        viewModelScope.launch {
+            flavors.collect()
+        }
+
+    }
 }
